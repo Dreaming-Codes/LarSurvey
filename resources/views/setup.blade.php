db538e10c390d3d112337f6987b22c6ca895a89d
Lorenzo Rizzotti <lore.rizzotti@gmail.com>
2020-06-09 11:54:18 +0200

    Seleziona la lingua solo se non già fatto (detect se il file .env contiene la lingua)

--- resources/views/setup.blade.php
+++ resources/views/setup.blade.php
@@ -16,25 +16,38 @@
         <p xmlns:dct="http://purl.org/dc/terms/" xmlns:cc="http://creativecommons.org/ns#" class="license-text"><a rel="cc:attributionURL" href="https://github.com/Dreaming-Codes/LarSurvey/"><span rel="dct:title">LarSurvey</span></a> by <a rel="cc:attributionURL" href="https://rizzotti.eu"><span rel="cc:attributionName">Lorenzo Rizzotti </span></a><a href="https://creativecommons.org/licenses/by-nc-sa/4.0">CC BY-NC-SA 4.0</a>
     </div>
 </footer>
+<?php
+$EnvLang = getenv ('APP_LOCALE');
+if (strlen($EnvLang) < 1) {
+    langSelect();
+} else {
+    setup();
+};
+
+function langSelect () {
+?>
 <div class="max-w-sm rounded overflow-hidden shadow-lg m-auto">
     <div class="px-6 py-4">
 <form action="<?php if (!empty($_POST)){file_put_contents("../.env",str_replace('APP_LOCALE=' . Lang::locale(),'APP_LOCALE=' . $_POST['lang'],file_get_contents("../.env"))); header("Refresh:0");} ?>" method="post">@csrf
     <label for="dbname">@lang('install.lang')</label><br>
     <select name="lang" id="lang">
-        <?php $languages = scandir("../resources/lang/"); unset($languages[0]); unset($languages[1]); $languages = array_values($languages); foreach($languages as $lang):;?>
+        <?php $languages = scandir("../resources/lang/"); unset($languages[0]); unset($languages[1]); $languages = array_values($languages); foreach($languages as $lang):?>
             <option value="<?= $lang ?>"><?= $lang ?></option>
         <?php endforeach; ?>
     </select>
+
     <br>
     <a>@lang('install.translationhelp')</a>
-        </p>
     </div>
     <div class="px-6 py-4">
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#LarSurvey</span>
         <input class="inline-block bg-gray-200 rounded-full px-4 py-2 text-sm font-semibold text-gray-700 mr-2" style="margin-left: 48%; margin-right: 2px;" type="submit">
     </div>
-    </form>
+</form>
 </div>
-</div>
+<?php };
+function setup () {?>
+Qui va il setup quando ho voglia di farlo
+<?php };?>
 </body>
 </html>
