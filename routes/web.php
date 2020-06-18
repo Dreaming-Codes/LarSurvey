c6dc963eddd33647eac40cff3f9187414f7177df
Lorenzo Rizzotti <lore.rizzotti@gmail.com>
2020-06-19 01:20:12 +0200

    Ora una volta finito il setup vieni inoltrato alla pagina welcome.

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-if (file_exists('../resources/views/setup.blade.php')){
+if (strlen(getenv('LAR_HOME')) < 1){
     Route::get('/', function () {
         return view('setup');
     });
